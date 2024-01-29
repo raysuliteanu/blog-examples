7be9a27dc836d882144fde716c0fb902a97422f4
Ray Suliteanu <raysuliteanu@gmail.com>
2024-01-29 15:05:54 -0800

    initial spring boot project
    generated a skeleton from start.spring.io using Gradle with support
    for separate integration test source set

---
+++ observability-demo/src/test/java/com/ksw/observability/demo/ObservabilityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksw.observability.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservabilityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
