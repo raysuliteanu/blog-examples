7278c9fa5caebcf61121ad0dd35e659773de8954
Ray Suliteanu <raysuliteanu@gmail.com>
2024-02-05 15:55:39 -0800

    initial commit

---
+++ aidemo/src/main/java/com/kidoni/aidemo/SpringBootAiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kidoni.aidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAiDemoApplication.class, args);
+    }
+
+}
---
+++ aidemo/src/test/java/com/kidoni/aidemo/SpringBootAiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kidoni.aidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
