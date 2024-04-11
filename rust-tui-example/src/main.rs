5464060c8f51f1bdbffa54214102223edf86efa3
Ray Suliteanu <raysuliteanu@gmail.com>
2024-04-11 12:29:54 -0700

    Update main function with terminal interaction
    The main function has been updated to include terminal interaction capabilities. Ratatui library is used to create a terminal UI and crossterm library is used for low-level terminal interactions. Also added supplementary functions to handle events, restore terminal state, and implement the terminal UI.

--- rust-tui-example/src/main.rs
+++ rust-tui-example/src/main.rs
@@ -1,3 +1,64 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::io::stdout;
+use std::panic::{set_hook, take_hook};
+use std::time::Duration;
+
+use crossterm::{event, ExecutableCommand};
+use crossterm::event::{Event, KeyCode};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen};
+use ratatui::{Frame, Terminal};
+use ratatui::{prelude::*, widgets::*};
+use ratatui::backend::CrosstermBackend;
+
+fn main() -> io::Result<()> {
+    init_panic_hook();
+    enable_raw_mode()?;
+    stdout().execute(EnterAlternateScreen)?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+
+    let mut should_quit = false;
+    while !should_quit {
+        terminal.draw(ui)?;
+        should_quit = handle_events()?;
+    }
+
+    restore_terminal()?;
+
+    Ok(())
+}
+
+fn init_panic_hook() {
+    let original_hook = take_hook();
+    set_hook(Box::new(move |panic_info| {
+        // intentionally ignore errors here since we're already in a panic
+        let _ = restore_terminal();
+        original_hook(panic_info);
+    }));
+}
+
+fn restore_terminal() -> io::Result<()> {
+    disable_raw_mode()?;
+    stdout().execute(LeaveAlternateScreen)?;
+    Ok(())
+}
+
+fn handle_events() -> io::Result<bool> {
+    if event::poll(Duration::from_millis(50))? {
+        if let Event::Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                return Ok(true);
+            }
+        }
+    }
+    Ok(false)
+}
+
+fn ui(frame: &mut Frame) {
+    let main_window = frame.size();
+
+    frame.render_widget(
+        Block::new().borders(Borders::all())
+            .title_style(Style::default().fg(Color::Blue).add_modifier(Modifier::BOLD))
+            .title("Something"),
+        main_window);
 }
