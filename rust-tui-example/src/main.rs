a869bfc275a9712d3558c9477d1acabe93505325
Ray Suliteanu <raysuliteanu@gmail.com>
2024-04-11 18:12:27 -0700

    Implement file reading capability
    Added functionality to read file data given the path from command-line arguments. The refactored main function now initializes terminal, runs the program using the read file data, and restores terminal settings upon completion. The user interface is also updated to display the file name and its contents.

--- rust-tui-example/src/main.rs
+++ rust-tui-example/src/main.rs
@@ -1,6 +1,9 @@
+use std::env::args;
+use std::error::Error;
 use std::io;
-use std::io::stdout;
+use std::io::{stdout, Stdout};
 use std::panic::{set_hook, take_hook};
+use std::path::Path;
 use std::time::Duration;
 
 use crossterm::{event, ExecutableCommand};
@@ -10,21 +13,67 @@ use ratatui::{Frame, Terminal};
 use ratatui::{prelude::*, widgets::*};
 use ratatui::backend::CrosstermBackend;
 
-fn main() -> io::Result<()> {
-    init_panic_hook();
-    enable_raw_mode()?;
-    stdout().execute(EnterAlternateScreen)?;
-    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+struct FileData {
+    path: String,
+    data: Vec<String>,
+}
 
-    let mut should_quit = false;
-    while !should_quit {
-        terminal.draw(ui)?;
-        should_quit = handle_events()?;
-    }
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = args().collect();
+
+    let mut file_data = get_file_data(args)?;
+
+    init_panic_hook();
+
+    let terminal = setup_terminal()?;
+
+    run(terminal, &mut file_data)?;
 
     restore_terminal()?;
 
     Ok(())
+}
+
+fn get_file_data(args: Vec<String>) -> Result<FileData, Box<dyn Error>> {
+    if args.len() == 2 && !args[1].is_empty() {
+        let path = args[1].clone();
+        let file_path = Path::new(path.as_str());
+
+        if file_path.exists() && file_path.is_file() {
+            let contents = std::fs::read_to_string(file_path).expect("could not read file");
+            println!("read {} characters", contents.len());
+            let data = contents.split('\n')
+                .map(|x| { x.to_string() })
+                .collect::<Vec<String>>();
+
+            Ok(FileData {
+                path: file_path.file_name().unwrap().to_str().unwrap().to_string(),
+                data,
+            })
+        } else {
+            panic!("file does not exist or cannot be read")
+        }
+    } else {
+        panic!("missing file name argument")
+    }
+}
+
+fn run(mut terminal: Terminal<CrosstermBackend<Stdout>>, file_data: &mut FileData) -> Result<(), Box<dyn Error>> {
+    let mut should_quit = false;
+    while !should_quit {
+        terminal.draw(|f| ui(f, file_data))?;
+        should_quit = handle_events()?;
+    }
+
+    Ok(())
+}
+
+fn setup_terminal() -> Result<Terminal<CrosstermBackend<Stdout>>, Box<dyn Error>> {
+    enable_raw_mode()?;
+    stdout().execute(EnterAlternateScreen)?;
+    let terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+
+    Ok(terminal)
 }
 
 fn init_panic_hook() {
@@ -53,7 +102,7 @@ fn handle_events() -> io::Result<bool> {
     Ok(false)
 }
 
-fn ui(frame: &mut Frame) {
+fn ui(frame: &mut Frame, file_data: &mut FileData) {
     let chunks = Layout::default()
         .direction(Direction::Vertical)
         .constraints([
@@ -66,13 +115,26 @@ fn ui(frame: &mut Frame) {
     let header_block = Block::new()
         .borders(Borders::all())
         .title_style(Style::default().fg(Color::Blue).add_modifier(Modifier::BOLD))
-        .title("Will be the file name");
-    frame.render_widget(header_block, chunks[0]);
+        .title("File Viewer");
 
-    let main_content = Block::new()
-        .borders(Borders::LEFT | Borders::RIGHT);
+    let file_name = ratatui::text::Line::from(file_data.path.clone())
+        .style(Style::default().fg(Color::Blue));
+    let header_content = Paragraph::new(file_name)
+        .centered()
+        .block(header_block);
+    frame.render_widget(header_content, chunks[0]);
+
+    let main_content_block = Block::new().borders(Borders::LEFT | Borders::RIGHT);
+
+    let text : Vec<Line>= file_data.data.iter()
+        .map(|line| { Line::from(line.to_string())})
+        .collect();
+
+    let main_content = Paragraph::new(text)
+        .block(main_content_block)
+        .wrap(Wrap { trim: false });
     frame.render_widget(main_content, chunks[1]);
-    
+
     let footer_block = Block::new()
         .borders(Borders::all());
 
