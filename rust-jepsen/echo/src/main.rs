3907f81e0f97c3d64e3ce9c76c6d50fa6958bb51
Ray Suliteanu <raysuliteanu@gmail.com>
2024-06-20 19:00:49 -0700

    echo example
    implement the echo challenge from https://fly.io/dist-sys/1/. Mostly
    working except some issue with the maelstrom runnner perhaps due to
    the extra escapes getting generated.

--- rust-jepsen/echo/src/main.rs
+++ rust-jepsen/echo/src/main.rs
@@ -1,3 +1,121 @@
+use std::io::{Stdout, Write};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct InitMessage {
+    src: String,
+    dest: String,
+    body: InitBody,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
+enum InitBody {
+    Request(InitRequestBody),
+    Response(InitResponseBody),
+}
+
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+struct InitRequestBody {
+    // can't use 'type' as the field name since it's a Rust keyword
+    #[serde(rename = "type")]
+    msg_type: String,
+    msg_id: u32,
+    node_id: String,
+    node_ids: Vec<String>,
+}
+
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+struct InitResponseBody {
+    // can't use 'type' as the field name since it's a Rust keyword
+    #[serde(rename = "type")]
+    msg_type: String,
+    in_reply_to: u32,
+}
+
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+struct EchoMessage {
+    src: String,
+    dest: String,
+    body: EchoBody,
+}
+
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+struct EchoBody {
+    // can't use 'type' as the field name since it's a Rust keyword
+    #[serde(rename = "type")]
+    msg_type: String,
+    msg_id: u32,
+    #[serde(skip_deserializing)]
+    in_reply_to: u32,
+    echo: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let stdout = &std::io::stdout();
+    let stdin = &mut std::io::stdin();
+    let mut reader = serde_json::Deserializer::from_reader(stdin);
+
+    let (reply_to, node_name, msg_id) = if let Ok(init) = InitMessage::deserialize(&mut reader).map_err(|e| dbg!(e)) {
+        match init.body {
+            InitBody::Request(i) => {
+                assert_eq!("init", i.msg_type);
+                (init.src, i.node_id, i.msg_id)
+            }
+            InitBody::Response(_) => {
+                panic!("init protocol failure: expected 'init' got 'init_ok'")
+            }
+        }
+    } else {
+        // failed deserializing init request
+        todo!()
+    };
+
+    let init_resp = InitMessage {
+        src: node_name.clone(),
+        dest: reply_to,
+        body: InitBody::Response(InitResponseBody {
+            msg_type: String::from("init_ok"),
+            in_reply_to: msg_id,
+        }),
+    };
+
+    let _ = serde_json::to_string(&init_resp)
+        .and_then(|s| {
+            write_response_message(stdout, s);
+            Ok(())
+        });
+
+    let mut id = 1u32;
+
+    loop {
+        if let Ok(message) = EchoMessage::deserialize(&mut reader).map_err(|e| dbg!(e)) {
+            let repl = EchoMessage {
+                src: node_name.clone(),
+                dest: message.src.clone(),
+                body: EchoBody {
+                    msg_type: String::from("echo_reply"),
+                    msg_id: id,
+                    in_reply_to: message.body.msg_id,
+                    echo: message.body.echo.clone(),
+                },
+            };
+
+            let _ = serde_json::to_string(&repl)
+                .and_then(|s| {
+                    write_response_message(stdout, s);
+                    Ok(())
+                });
+
+            id += 1;
+        }
+    }
+}
+
+fn write_response_message(mut stdout: &Stdout, resp: String) {
+    let res = serde_json::to_writer(stdout, &resp);
+    let _ = stdout.flush();
+    if let Some(write_error) = res.err() {
+        eprintln!("{write_error}");
+    }
 }
