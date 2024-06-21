84ad49d13176f8fd42950cee44ce5c297f0c88d4
Ray Suliteanu <raysuliteanu@gmail.com>
2024-06-21 14:17:38 -0700

    since I want to focus on the challenge and not the maelstrom runtime, switch to use the cargo crate maelstrom-node and refactor

--- rust-jepsen/echo/src/main.rs
+++ rust-jepsen/echo/src/main.rs
@@ -1,109 +1,36 @@
-use std::io::{Stdout, Write};
-use serde::{Deserialize, Serialize};
+use async_trait::async_trait;
+use maelstrom::protocol::Message;
+use maelstrom::{done, Node, Result, Runtime};
+use std::sync::Arc;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct InitRequestMessage {
-    src: String,
-    dest: String,
-    body: InitRequestBody,
+pub fn main() -> Result<()> {
+    Runtime::init(try_main())
 }
 
-#[derive(Default, Debug, Clone, Serialize, Deserialize)]
-struct InitRequestBody {
-    // can't use 'type' as the field name since it's a Rust keyword
-    #[serde(rename = "type")]
-    msg_type: String,
-    msg_id: u32,
-    node_id: String,
-    node_ids: Vec<String>,
+async fn try_main() -> Result<()> {
+    let handler = Arc::new(EchoHandler::default());
+    Runtime::new().with_handler(handler).run().await
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct InitResponseMessage {
-    src: String,
-    dest: String,
-    body: InitResponseBody,
-}
+#[derive(Clone, Default)]
+struct EchoHandler {}
 
-#[derive(Default, Debug, Clone, Serialize, Deserialize)]
-struct InitResponseBody {
-    // can't use 'type' as the field name since it's a Rust keyword
-    #[serde(rename = "type")]
-    msg_type: String,
-    in_reply_to: u32,
-}
+const ECHO_MSG_TYPE: &str = "echo";
+const ECHO_REPLY_MSG_TYPE: &str = "echo_ok";
 
-#[derive(Default, Debug, Clone, Serialize, Deserialize)]
-struct EchoMessage {
-    src: String,
-    dest: String,
-    body: EchoBody,
-}
-
-#[derive(Default, Debug, Clone, Serialize, Deserialize)]
-struct EchoBody {
-    // can't use 'type' as the field name since it's a Rust keyword
-    #[serde(rename = "type")]
-    msg_type: String,
-    msg_id: u32,
-    #[serde(skip_deserializing)]
-    in_reply_to: u32,
-    echo: String,
-}
-
-fn main() {
-    let stdout = &std::io::stdout();
-    let stdin = &mut std::io::stdin();
-    let mut reader = serde_json::Deserializer::from_reader(stdin);
-
-    let (reply_to, node_name, msg_id) =
-        if let Ok(init) = InitRequestMessage::deserialize(&mut reader)
-            .map(|t| dbg!(t))
-            .map_err(|e| dbg!(e)) {
-            assert_eq!("init", init.body.msg_type);
-            (init.src, init.body.node_id, init.body.msg_id)
-        } else {
-            // failed deserializing init request
-            panic!("invalid init message");
-        };
-
-    let init_resp = InitResponseMessage {
-        src: node_name.clone(),
-        dest: reply_to,
-        body: InitResponseBody {
-            msg_type: String::from("init_ok"),
-            in_reply_to: msg_id,
-        },
-    };
-
-    write_serializable(stdout, dbg!(init_resp));
-
-    let mut id = 1u32;
-
-    loop {
-        if let Ok(message) = EchoMessage::deserialize(&mut reader).map_err(|e| dbg!(e)) {
-            let repl = EchoMessage {
-                src: node_name.clone(),
-                dest: message.src.clone(),
-                body: EchoBody {
-                    msg_type: String::from("echo_reply"),
-                    msg_id: id,
-                    in_reply_to: message.body.msg_id,
-                    echo: message.body.echo.clone(),
-                },
-            };
-
-            write_serializable(stdout, repl);
-
-            id += 1;
+#[async_trait]
+impl Node for EchoHandler {
+    async fn process(&self, runtime: Runtime, request: Message) -> Result<()> {
+        let message_body = &request.body;
+        if message_body.typ == ECHO_MSG_TYPE && runtime.reply(request.clone(), message_body.clone()
+                .with_type(ECHO_REPLY_MSG_TYPE))
+                .await
+                .is_ok() {
+            
+            return Ok(())
         }
-    }
-}
 
-fn write_serializable(mut stdout: &Stdout, mesg: impl Serialize) {
-    let res = serde_json::to_writer(stdout, &mesg);
-    let _ = stdout.flush();
-    if let Some(write_error) = res.err() {
-        eprintln!("{write_error}");
+        done(runtime, request)
+        
     }
 }
