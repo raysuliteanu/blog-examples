64795567197aae1b6c24ad70e56d9cc6ed578171
Ray Suliteanu <raysuliteanu@gmail.com>
2024-06-27 17:40:38 -0700

    Update echo service and clean up dependencies
    Renamed the project to 'rust-flyio-jepsen-echo' and removed unused dependencies 'serde' and 'serde_json' from Cargo.toml. Also, made code readability improvements in the 'process' function of the EchoHandler implementation in 'main.rs', enhancing the overall maintainability of the code.

--- rust-jepsen/echo/src/main.rs
+++ rust-jepsen/echo/src/main.rs
@@ -1,3 +1,4 @@
+//! See https://fly.io/dist-sys/1/
 use async_trait::async_trait;
 use maelstrom::protocol::Message;
 use maelstrom::{done, Node, Result, Runtime};
@@ -22,15 +23,18 @@ const ECHO_REPLY_MSG_TYPE: &str = "echo_ok";
 impl Node for EchoHandler {
     async fn process(&self, runtime: Runtime, request: Message) -> Result<()> {
         let message_body = &request.body;
-        if message_body.typ == ECHO_MSG_TYPE && runtime.reply(request.clone(), message_body.clone()
-                .with_type(ECHO_REPLY_MSG_TYPE))
+        if message_body.typ == ECHO_MSG_TYPE
+            && runtime
+                .reply(
+                    request.clone(),
+                    message_body.clone().with_type(ECHO_REPLY_MSG_TYPE),
+                )
                 .await
-                .is_ok() {
-            
-            return Ok(())
+                .is_ok()
+        {
+            return Ok(());
         }
 
         done(runtime, request)
-        
     }
 }
