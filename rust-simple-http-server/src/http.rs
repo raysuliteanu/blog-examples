8136e2ea3cd35dc3147f053eea1a573554828f40
Ray Suliteanu <raysuliteanu@gmail.com>
2024-07-11 12:07:35 -0700

    hook up parser with processing loop
    finish refactor to use nom, hooking up the new parsing with the original loop handling

---
+++ rust-simple-http-server/src/http.rs
@@ -0,0 +1,275 @@
+use std::fmt::{Display, Formatter};
+use std::str::FromStr;
+
+use nom::bytes::complete::{tag, take, take_until, take_while1};
+use nom::character::complete::{char, multispace0, multispace1};
+use nom::character::{is_alphabetic, is_alphanumeric, is_digit, is_space};
+use nom::sequence::{delimited, preceded, separated_pair, terminated, tuple};
+use nom::IResult;
+
+use crate::http::HttpMethod::{Delete, Get, Head, Option, Post, Put};
+
+const END_OF_INPUT: &str = "\r\n\r\n";
+
+pub struct MediaType(pub(crate) &'static [u8]);
+
+impl MediaType {
+    pub const TEXT_PLAIN: MediaType = MediaType(b"text/plain");
+    pub const APPLICATION_OCTET_STREAM: MediaType = MediaType(b"application/octet-stream");
+}
+
+impl Display for MediaType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", to_string(self.0))
+    }
+}
+
+impl From<MediaType> for &str {
+    fn from(value: MediaType) -> Self {
+        to_string(value.0)
+    }
+}
+
+impl FromStr for MediaType {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "text/plain" => Ok(MediaType::TEXT_PLAIN),
+            "application/octet-stream" => Ok(MediaType::APPLICATION_OCTET_STREAM),
+            _ => Err(()),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct HttpHeader<'a> {
+    pub(crate) name: &'a [u8],
+    pub(crate) value: &'a [u8],
+}
+
+impl<'a> HttpHeader<'a> {
+    pub fn new(name: &'a [u8], value: &'a [u8]) -> Self {
+        HttpHeader { name, value }
+    }
+}
+
+impl Display for HttpHeader<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}: {}", to_string(self.name), to_string(self.value))
+    }
+}
+
+pub enum HttpMethod {
+    Get,
+    Put,
+    Post,
+    Delete,
+    Head,
+    Option,
+}
+
+impl FromStr for HttpMethod {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "GET" => Ok(Get),
+            "PUT" => Ok(Put),
+            "POST" => Ok(Post),
+            "DELETE" => Ok(Delete),
+            "HEAD" => Ok(Head),
+            "OPTION" => Ok(Option),
+            _ => Err(()),
+        }
+    }
+}
+
+pub(crate) struct HttpStatus(pub(crate) u16, &'static str, &'static str);
+pub(crate) const HTTP_OK: HttpStatus = HttpStatus(200, "OK", "The request has succeeded.");
+pub(crate) const _HTTP_CREATED: HttpStatus = HttpStatus(
+    201,
+    "Created",
+    "The request has been fulfilled and resulted in a new resource being created.",
+);
+pub(crate) const _HTTP_BAD_REQUEST: HttpStatus = HttpStatus(
+    400,
+    "Bad Request",
+    "The request could not be understood by the server due to malformed syntax.",
+);
+pub(crate) const HTTP_NOT_FOUND: HttpStatus = HttpStatus(
+    404,
+    "Not Found",
+    "The server has not found anything matching the Request-URI.",
+);
+pub(crate) const HTTP_METHOD_NOT_ALLOWED: HttpStatus = HttpStatus(
+    405,
+    "Method Not Allowed", 
+    "The method specified in the Request-Line is not allowed for the resource identified by the Request-URI."
+);
+
+pub struct HttpRequest<'r> {
+    pub method: HttpMethod,
+    pub path: &'r str,
+    pub version: &'r str,
+    pub headers: Vec<HttpHeader<'r>>,
+    pub body: &'r [u8],
+}
+
+pub fn parse_message(buffer: &[u8]) -> IResult<(&[u8]), HttpRequest> {
+    let (rest, preamble) = take_until(END_OF_INPUT)(buffer)?;
+    let (header_bytes, (method, path, version)) = parse_request_line(preamble)?;
+    let (_should_be_empty, headers) = read_headers(header_bytes)?;
+    assert!(_should_be_empty.is_empty());
+    assert!(rest.len() >= END_OF_INPUT.len());
+    let (body, _) = take(END_OF_INPUT.len())(rest)?;
+    Ok((
+        body,
+        HttpRequest {
+            method: HttpMethod::from_str(method).unwrap(),
+            path,
+            version,
+            headers,
+            body,
+        },
+    ))
+}
+
+fn parse_request_line(buffer: &[u8]) -> IResult<&[u8], (&str, &str, &str)> {
+    let method_parser = terminated(take_while1(is_alphabetic), multispace1);
+    let path_parser = terminated(take_while1(|b| b != b' '), multispace1);
+    let version_parser = take_while1(|b| is_digit(b) || b == b'.');
+    let http_version_parser = terminated(preceded(tag("HTTP/"), version_parser), match_eol);
+
+    tuple((method_parser, path_parser, http_version_parser))(buffer).map(|(rest, (m, p, v))| {
+        let method = to_string(m);
+        let path = to_string(p);
+        let version = to_string(v);
+        Ok((rest, (method, path, version)))
+    })?
+}
+
+fn read_headers(buffer: &[u8]) -> IResult<&[u8], Vec<HttpHeader>> {
+    let mut headers = Vec::new();
+
+    // and then parse each header
+    let mut rest = buffer;
+    while !rest.is_empty() {
+        match parse_header(rest) {
+            Ok((r, header)) => {
+                headers.push(header);
+                rest = r;
+            }
+            Err(e) => {
+                println!("{:?}", e);
+                return Err(e);
+            }
+        }
+    }
+
+    // r should just be the "\r\n\r\n"
+    Ok((rest, headers))
+}
+
+fn parse_header(input: &[u8]) -> IResult<&[u8], HttpHeader> {
+    let match_header_name = take_while1(|b| is_alphabetic(b) || b == b'-');
+    let match_header_value = take_while1(|b| {
+        is_alphanumeric(b) || is_space(b)
+            || b == b':'
+            || b == b'-'
+            || b == b'/'
+            || b == b'.'
+            || b == b'*'
+            || b == b','
+            || b == b';'
+            || b == b'='
+    });
+
+    let mut match_header = separated_pair(
+        match_header_name,
+        char(':'),
+        delimited(multispace1, match_header_value, multispace0),
+    );
+
+    let (rest, (name, value)) = match_header(input)?;
+
+    Ok((rest, HttpHeader { name, value }))
+}
+
+// path could be "" or "/" or "/foo" or "/foo/bar/baz" // path could be "" or "/" or "/foo" or "/foo/bar/baz"
+pub fn split_path(path: &str) -> (&str, &str) {
+    let try_split = path.strip_prefix('/');
+    match try_split {
+        Some(split) => {
+            if let Some(parts) = split.split_once('/') {
+                parts
+            } else {
+                (split, "")
+            }
+        }
+        None => (path, ""),
+    }
+}
+
+fn match_eol(input: &[u8]) -> IResult<&[u8], &[u8]> {
+    tag("\r\n")(input)
+}
+
+fn to_string(bytes: &[u8]) -> &str {
+    unsafe { std::str::from_utf8_unchecked(bytes) }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_preamble() {
+        let input = b"GET /foo/bar HTTP/1.1\r\nContent-Type: text/*\r\nContent-Length: 1234\r\n\r\nblahblah";
+        let res = parse_message(input);
+        assert!(res.is_ok());
+        let (rest, req) = res.unwrap();
+        assert_eq!(b"blahblah", rest);
+        assert_eq!("/foo/bar", req.path);
+    }
+
+    #[test]
+    fn test_parse_request_line() {
+        let input = b"GET /foo/bar HTTP/1.1\r\n";
+        let res = parse_request_line(input);
+        assert!(res.is_ok());
+        let (rest, (method, path, version)) = res.unwrap();
+        assert!(rest.is_empty());
+        assert_eq!("GET", method);
+        assert_eq!("/foo/bar", path);
+        assert_eq!("1.1", version);
+    }
+
+    #[test]
+    fn test_parse_header() {
+        let input = b"Content-Type: text/*\r\n";
+        let res = parse_header(input);
+        assert!(res.is_ok());
+        let (rest, header) = res.unwrap();
+        assert!(rest.is_empty());
+        assert_eq!(b"Content-Type", header.name);
+        assert_eq!(b"text/*", header.value);
+    }
+
+    #[test]
+    fn test_read_headers() {
+        let input = b"Content-Type: text/*\r\nContent-Length: 1234";
+        let res = read_headers(input);
+        assert!(res.is_ok());
+        let (rest, headers) = res.unwrap();
+        assert!(rest.is_empty());
+        assert_eq!(2, headers.len());
+        let h = &headers[0];
+        assert_eq!(b"Content-Type", h.name);
+        assert_eq!(b"text/*", h.value);
+
+        let h = &headers[1];
+        assert_eq!(b"Content-Length", h.name);
+        assert_eq!(b"1234", h.value);
+    }
+}
--- rust-simple-http-server/src/main.rs
+++ rust-simple-http-server/src/main.rs
@@ -1,127 +1,20 @@
-use std::collections::HashMap;
-use std::fmt::{Display, Formatter};
 use std::io;
 use std::io::{Error, ErrorKind};
 use std::path::PathBuf;
-use std::pin::pin;
-use std::str::FromStr;
 
 use clap::Parser;
 use itertools::Itertools;
+use nom::AsBytes;
 use tokio::fs::File;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, Interest};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::task_local;
 
-const CR_LF: &str = "\r\n";
-const HTTP11_PROTOCOL_STR: &str = "HTTP/1.1";
+use crate::http::{HttpHeader, HttpMethod, HttpStatus, MediaType, parse_message};
+
+mod http;
+
 const LOCALHOST: &str = "127.0.0.1";
-const READ_BUFFER_SIZE: usize = 4096;
-
-pub struct HttpStatusCode {
-    pub code: u16,
-    pub mesg: &'static str,
-}
-
-impl HttpStatusCode {
-    pub const HTTP_OK: HttpStatusCode = HttpStatusCode { code: 200, mesg: "OK" };
-    pub const HTTP_NOT_FOUND: HttpStatusCode = HttpStatusCode { code: 404, mesg: "Not Found" };
-    pub const HTTP_NOT_IMPLEMENTED: HttpStatusCode = HttpStatusCode { code: 501, mesg: "Not Implemented" };
-}
-
-impl Display for HttpStatusCode {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.write_fmt(format_args!("{} {}", self.code, self.mesg))
-    }
-}
-
-#[derive(PartialEq)]
-pub struct HttpMethod(&'static str);
-
-impl HttpMethod {
-    pub const GET: HttpMethod = HttpMethod("GET");
-    pub const PUT: HttpMethod = HttpMethod("PUT");
-    pub const POST: HttpMethod = HttpMethod("POST");
-    pub const DELETE: HttpMethod = HttpMethod("DELETE");
-}
-
-impl FromStr for HttpMethod {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<HttpMethod, ()> {
-        match s {
-            "GET" => Ok(HttpMethod::GET),
-            "PUT" => Ok(HttpMethod::PUT),
-            "POST" => Ok(HttpMethod::POST),
-            "DELETE" => Ok(HttpMethod::DELETE),
-            _ => Err(())
-        }
-    }
-}
-
-impl From<&[u8]> for HttpMethod {
-    fn from(value: &[u8]) -> Self {
-        let s = std::str::from_utf8(value);
-        HttpMethod::from_str(s.unwrap()).unwrap()
-    }
-}
-
-#[derive(PartialEq)]
-pub struct MediaType(&'static str);
-
-impl MediaType {
-    pub const TEXT_PLAIN: MediaType = MediaType("text/plain");
-    pub const APPLICATION_OCTET_STREAM: MediaType = MediaType("application/octet-stream");
-}
-
-impl Into<&'static str> for MediaType {
-    fn into(self) -> &'static str {
-        self.0
-    }
-}
-
-impl Display for MediaType {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-impl FromStr for MediaType {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "text/plain" => Ok(MediaType::TEXT_PLAIN),
-            "application/octet-stream" => Ok(MediaType::APPLICATION_OCTET_STREAM),
-            _ => Err(())
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct HttpHeader<'a> {
-    name: &'a str,
-    value: &'a str,
-}
-
-impl<'a> HttpHeader<'a> {
-    pub fn new(n: &'a [u8], v: &'a [u8]) -> Self {
-        let name= std::str::from_utf8(n).unwrap();
-        let value = std::str::from_utf8(v).unwrap();
-
-        HttpHeader { name, value }
-    }
-
-    pub fn new_from_str(name: &'a str, value: &'a str) -> Self {
-        HttpHeader { name, value }
-    }
-}
-
-impl Display for HttpHeader<'_> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}: {}", self.name, self.value)
-    }
-}
 
 #[derive(Parser)]
 struct Cli {
@@ -148,226 +41,6 @@ task_local! {
     static CONTEXT : ThreadLocalContext;
 }
 
-struct HttpRequest<'a> {
-    proto: &'a str,
-    raw_buf: Vec<u8>,
-    io_stream: &'a mut TcpStream,
-    http_method: Option<HttpMethod>,
-    request_path: &'a str,
-    headers: HashMap<&'a str, HttpHeader<'a>>,
-}
-
-impl<'a, 'b> HttpRequest<'a> {
-    fn new(stream: &'a mut TcpStream) -> Self {
-        HttpRequest {
-            raw_buf: Vec::with_capacity(READ_BUFFER_SIZE),
-            io_stream: stream,
-            http_method: None,
-            proto: HTTP11_PROTOCOL_STR,
-            request_path: "/",
-            headers: HashMap::default(),
-        }
-    }
-
-    async fn read_line(&mut self) -> io::Result<(usize, usize)> {
-        let start = self.raw_buf.len();
-        let mut read = 0usize;
-        loop {
-            let octet = self.io_stream.read_u8().await?;
-            if octet != 0x0d {
-                self.raw_buf.to_vec().push(octet);
-            } else {
-                let possible_nl = self.io_stream.read_u8().await?;
-                if possible_nl == 0x0a {
-                    // got 0x0d0a which is CR_NL
-                    break;
-                } else {
-                    // just a lone \r followed by some other byte, which seems strange but is legal I think
-                    self.raw_buf.to_vec().push(possible_nl);
-                }
-            }
-
-            read += 1;
-        }
-
-        Ok((start, read))
-    }
-
-    async fn parse_request_line(&mut self) -> io::Result<()> {
-        let _ = self.read_line().await?;
-
-        let mut parts = self.raw_buf.split(|b| {
-            *b == ' ' as u8
-        });
-
-        let method = parts.next();
-        let path = parts.next();
-        let proto = parts.next();
-
-        if method.is_none() || path.is_none() || proto.is_none() {
-            todo!()
-        }
-
-        // only support HTTP/1.1
-        if self.proto != std::str::from_utf8(proto.unwrap()).unwrap() {
-            todo!()
-        }
-
-        let http_method = HttpMethod::from(method.unwrap());
-        self.http_method = Some(http_method);
-        // self.request_path = path.unwrap();
-
-        Ok(())
-    }
-
-    // path could be "" or "/" or "/foo" or "/foo/bar/baz"
-    fn parse_path(&mut self) -> (&'a str, &'a str) {
-        let try_split = self.request_path.strip_prefix('/');
-        match try_split {
-            Some(split) => {
-                if let Some(parts) = split.split_once('/') {
-                    parts
-                } else {
-                    (split, "")
-                }
-            }
-            None => (self.request_path, "")
-        }
-    }
-
-    async fn parse_request_header(&mut self) -> io::Result<Option<HttpHeader<'a>>> {
-        let (start, read) = self.read_line().await?;
-        let header_line = &mut self.raw_buf[start..read];
-
-        if read > 0 {
-            if let Some(index) = header_line.into_iter().position(|b| *b == b':') {
-                let (n, v) = header_line.split_at(index);
-                Ok(Some(HttpHeader::new(n, v)))
-            } else {
-                // invalid header line, since no ':'
-                todo!()
-            }
-        } else {
-            Ok(None)
-        }
-    }
-
-    async fn handle_files(&mut self, filename: &str) -> io::Result<()> {
-        let result = read_file(filename).await;
-        if let Err(_) = result {
-            return self.handle_not_found().await;
-        }
-
-        let contents = result.unwrap();
-
-        let mut buffer = Vec::new();
-        build_protocol_header(&mut buffer, &HttpStatusCode::HTTP_OK);
-
-        let content_type = &HttpHeader::new_from_str("Content-Type", MediaType::APPLICATION_OCTET_STREAM.into());
-        let len = contents.len().to_string();
-        let content_length = &HttpHeader::new_from_str("Content-Length", len.as_str());
-
-        build_headers(&mut buffer, vec![content_type, content_length]);
-
-        build_response_body(&mut buffer, &contents[..]);
-
-        self.io_stream.write(&buffer).await?;
-
-        Ok(())
-    }
-
-    async fn handle_not_found(&mut self) -> io::Result<()> {
-        let mut buffer = Vec::new();
-        build_protocol_header(&mut buffer, &HttpStatusCode::HTTP_NOT_FOUND);
-
-        let content_length: HttpHeader = HttpHeader::new_from_str("Content-Length", "0");
-        build_headers(&mut buffer, vec![&content_length]);
-
-        self.io_stream.write(&buffer).await?;
-
-        Ok(())
-    }
-
-    async fn handle_not_implemented(&mut self) -> io::Result<()> {
-        let mut buffer = Vec::new();
-        build_protocol_header(&mut buffer, &HttpStatusCode::HTTP_NOT_IMPLEMENTED);
-
-        let content_length: HttpHeader = HttpHeader::new_from_str("Content-Length", "0");
-        build_headers(&mut buffer, vec![&content_length]);
-
-        self.io_stream.write(&buffer).await?;
-
-        Ok(())
-    }
-
-    async fn handle_static_content(&mut self, _path: &str) -> io::Result<()> {
-        let mut buffer = Vec::new();
-        build_protocol_header(&mut buffer, &HttpStatusCode::HTTP_OK);
-
-        let content_length: HttpHeader = HttpHeader::new_from_str("Content-Length", "0");
-        build_headers(&mut buffer, vec![&content_length]);
-
-        build_response_body(&mut buffer, "".as_bytes());
-
-        self.io_stream.write(&buffer).await?;
-
-        Ok(())
-    }
-
-    async fn handle_echo(&mut self, body: &[u8]) -> io::Result<()> {
-        let mut buffer = Vec::new();
-        build_protocol_header(&mut buffer, &HttpStatusCode::HTTP_OK);
-
-        let content_type = &HttpHeader::new_from_str("Content-Type", MediaType::TEXT_PLAIN.into());
-        let len = body.len().to_string();
-        let content_length = &HttpHeader::new_from_str("Content-Length", len.as_str());
-
-        build_headers(&mut buffer, vec![content_type, content_length]);
-
-        build_response_body(&mut buffer, body);
-
-        self.io_stream.write(&buffer).await?;
-
-        Ok(())
-    }
-
-    async fn execute_request(&mut self) -> io::Result<()> {
-        self.parse_request_line().await?;
-
-        while let Some(header) = self.parse_request_header().await? {
-            self.headers.insert(header.name, header);
-        }
-
-        let (base_path, remaining_path_parts) = self.parse_path();
-
-        if let Some(m) = &mut self.http_method {
-            match m {
-                &mut HttpMethod::GET => {
-                    match base_path {
-                        "" | "index.html" => self.handle_static_content(base_path).await?,
-                        "files" => self.handle_files(remaining_path_parts).await?,
-                        "echo" => self.handle_echo(remaining_path_parts.as_bytes()).await?,
-                        "user-agent" => {
-                            if let Some(user_agent_header) = self.headers.get("User-Agent") {
-                                self.handle_echo(&user_agent_header.value.as_bytes()).await?
-                            } else {
-                                self.handle_not_found().await?
-                            }
-                        }
-                        _ => self.handle_not_found().await?,
-                    }
-                }
-                &mut HttpMethod::POST => {}
-                _ => self.handle_not_implemented().await?
-            }
-        } else {
-            self.handle_not_implemented().await?
-        }
-
-        Ok(())
-    }
-}
-
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> io::Result<()> {
     let (dir, port) = get_command_line_opts();
@@ -377,11 +50,7 @@ async fn main() -> io::Result<()> {
     loop {
         let (mut stream, _addr) = listener.accept().await?;
 
-        tokio::spawn(
-            CONTEXT.scope(context, async move {
-                process_request(&mut stream).await
-            })
-        );
+        tokio::spawn(CONTEXT.scope(context, async move { process_request(&mut stream).await }));
     }
 }
 
@@ -395,21 +64,91 @@ fn get_command_line_opts() -> (Option<&'static str>, u16) {
 }
 
 fn get_directory(dir_opt: Option<String>) -> Option<&'static str> {
-    if dir_opt.is_some() {
-        Some(Box::new(dir_opt.unwrap()).leak())
+    if let Some(dir) = dir_opt {
+        Some(Box::new(dir).leak())
     } else {
         None
     }
 }
 
 fn get_port(port_opt: Option<u16>) -> u16 {
-    port_opt.or_else(|| { Some(8080) }).unwrap()
+    port_opt.unwrap_or(8080)
 }
 
 async fn process_request(stream: &mut TcpStream) -> io::Result<()> {
-    let mut req = HttpRequest::new(stream);
-    let mut request = pin!(req);
-    request.execute_request().await?;
+    let buf = &mut Vec::<u8>::with_capacity(4096);
+    loop {
+        let ready = stream.ready(Interest::READABLE).await?;
+        if ready.is_readable() {
+            // Try to read data, this may still fail with `WouldBlock`
+            // if the readiness event is a false positive.
+            match stream.try_read_buf(buf) {
+                Ok(0) => break,
+                Ok(_n) => {
+                    // nothing to do rn
+                }
+                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                    break;
+                }
+                Err(e) => {
+                    return Err(e.into());
+                }
+            }
+        }
+    }
+
+    let (_, http_request) =
+        parse_message(buf).map_err(|e| std::io::Error::from(ErrorKind::InvalidData))?;
+
+    match http_request.method {
+        HttpMethod::Get => {
+            let (cmd, args) = http::split_path(http_request.path);
+            match cmd {
+                "" | "index.html" => handle_static_content(stream, cmd).await?,
+                "files" => handle_files(stream, args).await?,
+                "echo" => handle_echo(stream, args.as_bytes()).await?,
+                "user-agent" => {
+                    if let Some(user_agent_header) = http_request
+                        .headers
+                        .iter()
+                        .find(|h| h.name == b"User-Agent")
+                    {
+                        handle_echo(stream, &user_agent_header.value).await?
+                    } else {
+                        handle_not_found(stream).await?
+                    }
+                }
+                _ => handle_not_found(stream).await?,
+            }
+        }
+        HttpMethod::Put => {}
+        _ => handle_not_implemented(stream).await?,
+    }
+
+    Ok(())
+}
+
+async fn handle_files(stream: &mut TcpStream, filename: &str) -> io::Result<()> {
+    let result = read_file(filename).await;
+    if let Err(e) = result {
+        eprintln!("error reading {filename}: {e}");
+        return handle_not_found(stream).await;
+    }
+
+    let contents = result.unwrap();
+
+    let mut buffer = Vec::new();
+    build_protocol_header(&mut buffer, http::HTTP_OK);
+
+    let content_type = HttpHeader::new(b"Content-Type", MediaType::APPLICATION_OCTET_STREAM.0);
+    let len = contents.len().to_string();
+    let content_length = HttpHeader::new(b"Content-Length", len.as_bytes());
+
+    build_headers(&mut buffer, vec![content_type, content_length]);
+
+    build_response_body(&mut buffer, &contents[..]);
+
+    let _ = stream.write(&buffer).await?;
 
     Ok(())
 }
@@ -431,16 +170,70 @@ async fn read_file(filename: &str) -> io::Result<Vec<u8>> {
     }
 }
 
-fn build_protocol_header(buffer: &mut Vec<u8>, http_status_code: &HttpStatusCode) {
-    let protocol_line = format!("{HTTP11_PROTOCOL_STR} {}{CR_LF}", http_status_code);
+async fn handle_not_found(stream: &mut TcpStream) -> io::Result<()> {
+    let mut buffer = Vec::new();
+    build_protocol_header(&mut buffer, http::HTTP_NOT_FOUND);
+
+    build_headers(&mut buffer, vec![HttpHeader::new(b"Content-Length", b"0")]);
+
+    let _ = stream.write(&buffer).await?;
+
+    Ok(())
+}
+
+async fn handle_not_implemented(stream: &mut TcpStream) -> io::Result<()> {
+    let mut buffer = Vec::new();
+    build_protocol_header(&mut buffer, http::HTTP_METHOD_NOT_ALLOWED);
+
+    build_headers(&mut buffer, vec![HttpHeader::new(b"Content-Length", b"0")]);
+
+    let _ = stream.write(&buffer).await?;
+
+    Ok(())
+}
+
+async fn handle_static_content(stream: &mut TcpStream, _path: &str) -> io::Result<()> {
+    let mut buffer = Vec::new();
+    build_protocol_header(&mut buffer, http::HTTP_OK);
+
+    build_headers(&mut buffer, vec![HttpHeader::new(b"Content-Length", b"0")]);
+
+    build_response_body(&mut buffer, "".as_bytes());
+
+    let _ = stream.write(&buffer).await?;
+
+    Ok(())
+}
+
+async fn handle_echo<'a>(stream: &mut TcpStream, body: &[u8]) -> io::Result<()> {
+    let mut buffer = Vec::new();
+    build_protocol_header(&mut buffer, http::HTTP_OK);
+
+    let content_type = HttpHeader::new(b"Content-Type", MediaType::TEXT_PLAIN.0);
+
+    let len = body.len().to_string();
+    let content_length = HttpHeader::new(b"Content-Length", len.as_bytes());
+
+    build_headers(&mut buffer, vec![content_type, content_length]);
+
+    build_response_body(&mut buffer, body);
+
+    let _ = stream.write(&buffer).await?;
+
+    Ok(())
+}
+
+const CR_LF: &str = "\r\n";
+
+fn build_protocol_header(buffer: &mut Vec<u8>, http_status: HttpStatus) {
+    let protocol_line = format!("HTTP/1.1 {}{CR_LF}", http_status.0);
     buffer.append(&mut protocol_line.as_bytes().to_vec());
 }
 
-fn build_headers(buffer: &mut Vec<u8>, headers: Vec<&HttpHeader>) {
+fn build_headers(buffer: &mut Vec<u8>, headers: Vec<HttpHeader>) {
     let mut header_content = headers.iter()
-        .map(|http_header: &&HttpHeader| {
-            http_header.to_string()
-            // format!("{http_header}")
+        .map(|http_header: &HttpHeader| {
+            format!("{http_header}")
         })
         .collect_vec().join(CR_LF);
 
@@ -452,27 +245,4 @@ fn build_headers(buffer: &mut Vec<u8>, headers: Vec<&HttpHeader>) {
 
 fn build_response_body(buffer: &mut Vec<u8>, content: &[u8]) {
     buffer.append(&mut content.to_vec())
-}
-
-#[cfg(test)]
-mod tests {
-    /*    #[test]
-    fn test_parse_path() {
-        let (path, args) = parse_path("");
-        assert_eq!("", path);
-        assert_eq!(0, args.len());
-
-        let (path, args) = parse_path("/");
-        assert_eq!("", path);
-        assert_eq!(0, args.len());
-
-        let (path, args) = parse_path("/index.html");
-        assert_eq!("index.html", path);
-        assert_eq!(0, args.len());
-
-        let (path, args) = parse_path("/echo/foo/bar");
-        assert_eq!("echo", path);
-        assert_eq!("foo/bar", args);
-    }
-*/
 }
