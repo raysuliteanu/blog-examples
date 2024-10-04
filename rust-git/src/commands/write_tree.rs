34c08bb24d35bc9b5d81d0b9533ff20a0b5a8bd5
Ray Suliteanu <raysuliteanu@gmail.com>
2024-10-04 12:51:26 -0700

    additional write-tree work
    make sure the mode is saved correctly and the entries are sorted

--- rust-git/src/commands/write_tree.rs
+++ rust-git/src/commands/write_tree.rs
@@ -39,19 +39,25 @@ fn write_tree(path: PathBuf) -> GitResult<String> {
         let entry = entry?;
         let name = entry.file_name().to_string_lossy().to_string();
 
-        trace!("processing {:?}", name);
+        trace!("processing dir entry: '{name}'");
 
-        let tree_entry = if name != util::GIT_DIR_NAME && entry.metadata()?.is_dir() {
+        let tree_entry = if entry.metadata()?.is_dir() {
+            if name == util::GIT_DIR_NAME {
+                continue;
+            }
+
             let sha1 = write_tree(path.join(&name))?;
-            make_tree_entry(name, entry, sha1)?
+            make_tree_entry(name, entry, sha1, true)?
         } else {
             let mut file = std::fs::File::open(path.join(&name))?;
             let sha1 = hash_object::hash_object(&make_hash_object_args("blob"), &mut file)?;
-            make_tree_entry(name, entry, sha1)?
+            make_tree_entry(name, entry, sha1, false)?
         };
 
         tree.entries.push(tree_entry);
     }
+
+    tree.entries.sort_by(|x, y| x.name.cmp(&y.name));
 
     let mut entries: Vec<u8> = Vec::new();
     let mut size = 0;
@@ -83,10 +89,57 @@ fn make_hash_object_args(obj_type: &str) -> HashObjectArgs {
     }
 }
 
-fn make_tree_entry(name: String, entry: DirEntry, sha1: String) -> GitResult<TreeEntry> {
-    Ok(TreeEntry {
-        name,
-        mode: entry.metadata()?.mode().to_string(),
-        sha1,
-    })
+fn make_tree_entry(
+    name: String,
+    entry: DirEntry,
+    sha1: String,
+    is_tree: bool,
+) -> GitResult<TreeEntry> {
+    let raw_mode = entry.metadata()?.mode();
+    let mode = mode_to_string(raw_mode);
+    Ok(TreeEntry { name, mode, sha1 })
+}
+
+/// 32-bit mode, split into (high to low bits)
+///
+/// 4-bit object type
+///   valid values in binary are 1000 (regular file), 1010 (symbolic link)
+///   and 1110 (gitlink)
+///
+/// 3-bit unused
+///
+/// 9-bit unix permission. Only 0755 and 0644 are valid for regular files.
+/// Symbolic links and gitlinks have value 0 in this field.
+///
+/// 0100000000000000 (040 000): Directory
+/// 1000000110100100 (100 644): Regular non-executable file
+/// 1000000110110100 (100 664): Regular non-executable group-writeable file
+/// 1000000111101101 (100 755): Regular executable file
+/// 1010000000000000 (120 000): Symbolic link
+/// 1110000000000000 (160 000): Gitlink
+///
+/// https://stackoverflow.com/questions/737673/how-to-read-the-mode-field-of-git-ls-trees-output/8347325
+
+fn mode_to_string(mode: u32) -> String {
+    match mode & 0o170000 {
+        // if the type is dir or symlink, just use that; don't care about further permissions
+        0o040000 | 0o120000 => format!("{:0>6o}", mode & 0o170000),
+        // regular files, use the whole thing
+        0o100000 => format!("{:0>6o}", mode),
+        // everything else is invalid (e.g. block/char devices, pipes, etc)
+        _ => panic!("invalid mode: {:o}", mode),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_mode_to_string() {
+        assert_eq!("040000", mode_to_string(0o040755));
+        assert_eq!("120000", mode_to_string(0o120000));
+        assert_eq!("100755", mode_to_string(0o100755));
+        assert_eq!("100444", mode_to_string(0o100444));
+    }
 }
