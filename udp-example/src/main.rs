a9e12eb64f069c4353ac99031f275f7971ec8db2
Ray Suliteanu <raysuliteanu@gmail.com>
2025-04-01 11:29:26 -0700

    clean up example

--- udp-example/src/main.rs
+++ udp-example/src/main.rs
@@ -1,8 +1,8 @@
-use std::{error::Error, thread};
+use std::{error::Error, net::UdpSocket, thread};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let udp_client = std::net::UdpSocket::bind("127.0.0.1:0")?;
-    let udp_server = std::net::UdpSocket::bind("127.0.0.1:0")?;
+    let udp_client = UdpSocket::bind("127.0.0.1:0")?;
+    let udp_server = UdpSocket::bind("127.0.0.1:0")?;
     let addr = udp_server.local_addr()?;
     udp_client.connect(addr)?;
 
@@ -14,18 +14,18 @@ fn main() -> Result<(), Box<dyn Error>> {
         let (sz, addr) = udp_server
             .recv_from(&mut buf)
             .expect("error reading from client");
-        println!("read {sz} bytes from {addr}");
+        println!("server: read {sz} bytes");
         udp_server.send_to(&buf[..sz], addr)
     });
 
     let sz = udp_client.send_to("hello, world".as_bytes(), addr)?;
-    println!("wrote {sz} bytes to {addr}");
+    println!("client: wrote {sz} bytes");
 
     let mut buf = [0u8; 1024];
-    let (sz, addr) = udp_client
+    let (sz, _addr) = udp_client
         .recv_from(&mut buf)
         .expect("error reading from client");
-    println!("read {sz} bytes from {addr}");
+    println!("client: read {sz} bytes");
 
     Ok(())
 }
