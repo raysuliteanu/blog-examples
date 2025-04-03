ba6d3b287955e19b7302d331519f1062d88d4c16
Ray Suliteanu <raysuliteanu@gmail.com>
2025-04-03 12:57:26 -0700

    write typestate example

--- typestate/src/main.rs
+++ typestate/src/main.rs
@@ -1,3 +1,57 @@
+use std::default::Default;
+
+#[derive(Default, Clone)]
+pub struct Token;
+#[derive(Default, Clone)]
+pub struct Ast;
+
+#[derive(Default, Clone)]
+pub struct Scanner(&'static str);
+#[derive(Default, Clone)]
+pub struct Parser(Vec<Token>);
+#[derive(Default, Clone)]
+pub struct Evaluater(Vec<Ast>);
+#[derive(Default, Clone)]
+pub struct CompilerResult;
+
+#[derive(Default, Clone)]
+pub struct Compiler<S> {
+    stage: S,
+}
+
+impl Compiler<Scanner> {
+    pub fn new(source: &'static str) -> Self {
+        Compiler {
+            stage: Scanner(source),
+        }
+    }
+
+    pub fn scan(&self) -> Compiler<Parser> {
+        let _ = self.stage.0;
+        Compiler {
+            stage: Parser(Vec::new()),
+        }
+    }
+}
+
+impl Compiler<Parser> {
+    pub fn parse(&self) -> Compiler<Evaluater> {
+        for _t in &self.stage.0 {}
+        Compiler {
+            stage: Evaluater(Vec::new()),
+        }
+    }
+}
+
+impl Compiler<Evaluater> {
+    pub fn evaluate(&self) -> Compiler<CompilerResult> {
+        for _t in &self.stage.0 {}
+        Compiler {
+            stage: CompilerResult,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _compiler = Compiler::new("hello").scan().parse().evaluate();
 }
