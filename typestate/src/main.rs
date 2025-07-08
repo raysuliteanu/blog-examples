6269ab48afb33c6412b36d773038ebcf27f9e189
Ray Suliteanu <raysuliteanu@gmail.com>
2025-07-08 15:45:24 -0700

    Push wnznvmnmqkxr (#23)
    * new blog example for type state pattern
    
    initial commit of shell generated by 'cargo init'
    
    * write typestate example
    
    * cleanup
    
    * flesh out the type state example
    
    * enforce that build() can only be called once source is provided
    
    use the type state pattern in the builder as well, to ensure the
    only way to call CompilerBuilder.build() is if CompilerBuilder.source()
    has been called.
    
    * use the builder provided configuration
    
    optionally print the tokens and ast
    
    * simplify by combining impl blocks
    
    * cleanup slightly but in parituclary the build() method
    
    Prior to implementing the state pattern for the builder, we used a
    Result object with an if test in the method of the body, but one we did
    implement the state pattern, I forgot to remove that.
    
    Also simplified where I could by using `..self`
    
    * very minor cleanup
    
    * split into lib and bin 
    
    separate the compiler and builder into a separate module to validate
    that at each stage we can do only what we're supposed to, adjusting
    visibility appropriately.

--- typestate/src/main.rs
+++ typestate/src/main.rs
@@ -1,57 +1,18 @@
-use std::default::Default;
+use crate::compiler::CompilerBuilder;
 
-#[derive(Default, Clone)]
-pub struct Token;
-#[derive(Default, Clone)]
-pub struct Ast;
-
-#[derive(Default, Clone)]
-pub struct Scanner(&'static str);
-#[derive(Default, Clone)]
-pub struct Parser(Vec<Token>);
-#[derive(Default, Clone)]
-pub struct Evaluater(Vec<Ast>);
-#[derive(Default, Clone)]
-pub struct CompilerResult;
-
-#[derive(Default, Clone)]
-pub struct Compiler<S> {
-    stage: S,
-}
-
-impl Compiler<Scanner> {
-    pub fn new(source: &'static str) -> Self {
-        Compiler {
-            stage: Scanner(source),
-        }
-    }
-
-    pub fn scan(&self) -> Compiler<Parser> {
-        let _ = self.stage.0;
-        Compiler {
-            stage: Parser(Vec::new()),
-        }
-    }
-}
-
-impl Compiler<Parser> {
-    pub fn parse(&self) -> Compiler<Evaluater> {
-        for _t in &self.stage.0 {}
-        Compiler {
-            stage: Evaluater(Vec::new()),
-        }
-    }
-}
-
-impl Compiler<Evaluater> {
-    pub fn evaluate(&self) -> Compiler<CompilerResult> {
-        for _t in &self.stage.0 {}
-        Compiler {
-            stage: CompilerResult,
-        }
-    }
-}
+mod compiler;
 
 fn main() {
-    let _compiler = Compiler::new("hello").scan().parse().evaluate();
+    let compiler = CompilerBuilder::new()
+        .print_tokens()
+        .with_source("hello world this is a type state example")
+        .print_ast()
+        .build();
+
+    compiler.scan().parse().evaluate().finish();
+
+    let scanner = compiler.scan();
+    let parser = scanner.parse();
+    let ev = parser.evaluate();
+    ev.finish();
 }
